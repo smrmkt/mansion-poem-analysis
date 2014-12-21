c57372340e573e064603b59357025ebb76d3f525
smrmkt <smrmkt@gmail.com>
2014-12-21 20:03:04 +0900

    add execute file

---
+++ bin/get_similar_mantion_poem.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+#-*-coding:utf-8-*-
+
+import sys
+import os.path
+import argparse
+
+# path
+script_path = os.path.dirname(__file__)
+script_path = script_path if len(script_path) else '.'
+sys.path.append(script_path + '/../src')
+
+from poem_model import PoemModel
+from poem_lda_model import PoemLdaModel
+from poem_lsi_model import PoemLsiModel
+
+# args
+parser = argparse.ArgumentParser()
+parser.add_argument('sentence')
+
+if __name__ == '__main__':
+    sentence = parser.parse_args().sentence
+
+    model = PoemModel()
+    # model.construct()
+    model.load()
+    top_n = model.get_similar(sentence)
+    for k, v in top_n: print k, v
+
+    print '==='
+
+    lda_model = PoemLdaModel()
+    # lda_model.construct()
+    lda_model.load()
+    top_n = lda_model.get_similar(sentence)
+    for k, v in top_n: print k, v
+
+    print '==='
+
+    lsi_model = PoemLsiModel()
+    # lsi_model.construct()
+    lsi_model.load()
+    top_n = lsi_model.get_similar(sentence)
+    for k, v in top_n: print k, v
