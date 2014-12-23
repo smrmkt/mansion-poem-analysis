e0f93e883ebb43afbb438b9271d6f27b8b8f1126
smrmkt <smrmkt@gmail.com>
2014-12-23 13:48:56 +0900

    fix format

--- bin/get_similar_mantion_poem.py
+++ bin/get_similar_mantion_poem.py
@@ -20,25 +20,26 @@ parser.add_argument('sentence')
 
 if __name__ == '__main__':
     sentence = parser.parse_args().sentence
-
+    # standard tf-idf model
     model = PoemModel()
     # model.construct()
     model.load()
     top_n = model.get_similar(sentence)
     for k, v in top_n: print k, v
-
     print '==='
 
-    lda_model = PoemLdaModel()
-    # lda_model.construct()
-    lda_model.load()
-    top_n = lda_model.get_similar(sentence)
-    for k, v in top_n: print k, v
-
-    print '==='
-
+    # lsi model
     lsi_model = PoemLsiModel()
     # lsi_model.construct()
     lsi_model.load()
     top_n = lsi_model.get_similar(sentence)
     for k, v in top_n: print k, v
+    print '==='
+
+    # lda model
+    lda_model = PoemLdaModel()
+    lda_model.construct()
+    lda_model.load()
+    top_n = lda_model.get_similar(sentence)
+    for k, v in top_n: print k, v
+
